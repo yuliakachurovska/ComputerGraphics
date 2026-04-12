cd270b6df739faa666302261e1294538c0911aa1
yuliakachurovska <kachurovska167kv@gmail.com>
2026-04-12 18:48:24 +0300

    Add hw02 first image

--- OpenGL_Start/src/main.cpp
+++ OpenGL_Start/src/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -8,29 +10,117 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+
+    auto vertexShaderCode = R"(
+        #version 330 core
+
+        layout(location = 0) in vec4 aPos;
+
+        void main() {
+            gl_Position = aPos;
+        }
+    )";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderCode, nullptr);
+    glCompileShader(vertexShader);
+
+    auto fragmentShaderCode = R"(
+        #version 330 core
+        out vec4 FragColor;
+
+        void main() {
+            FragColor = vec4(0.0, 1.0, 0.0, 1.0);  // зелений
+       }
+    )";
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderCode, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Програма з шейдерів
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {  // float* vertices
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+          0.f,  0.5f,
+    };
+
+    GLuint VBO; // data - ідентифікатор для даних - місток CPU та GPU
+    GLuint VAO; // vertex array object
+
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind = activate
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+        0,                  // location - 0
+        2,                  // 2 компоненти: x, y
+        GL_FLOAT,           // тип даних
+        GL_FALSE,           // не нормалізувати
+        2 * sizeof(float),  // stride: 2 float-а на вершину
+        (void*)0            // offset: починаємо з 0
+    );
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0); // деактивувати VAO
+
+
+
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    do
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
-    }
+    } while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE));
+
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
+
 
     glfwTerminate();
     return 0;
---
+++ OpenGL_Start/src/task_02/main.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Colorful Rectangle", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+
+    auto vertexShaderCode = R"(
+        #version 330 core
+
+        layout(location = 0) in vec2 aPos;   // Координати
+        layout(location = 1) in vec3 aColor; // Колір
+
+        out vec3 ourColor; // Змінна для передачі кольору у фрагментний шейдер
+
+        void main() {
+            gl_Position = vec4(aPos, 0.0, 1.0);
+            ourColor = aColor; // Передаємо колір далі
+        }
+    )";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderCode, nullptr);
+    glCompileShader(vertexShader);
+
+    auto fragmentShaderCode = R"(
+        #version 330 core
+        
+        in vec3 ourColor; // Отримуємо проінтерпольований колір
+        out vec4 FragColor;
+
+        void main() {
+            FragColor = vec4(ourColor, 1.0); // Застосовуємо його
+       }
+    )";
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderCode, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Програма з шейдерів
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    float vertices[] = {
+        // Координати         // Кольори
+         0.5f,  0.5f,   1.0f, 0.0f, 0.0f,   // Вершина 1 - Червона
+         0.5f, -0.5f,   1.0f, 1.0f, 0.0f,   // Вершина 2 - Жовта
+        -0.5f, -0.5f,   0.0f, 1.0f, 0.0f,   // Вершина 3 - Зелена
+        -0.5f,  0.5f,   0.0f, 0.0f, 1.0f    // Вершина 4 - Синя
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,  // Перший трикутник
+        1, 2, 3   // Другий трикутник
+    };
+
+    GLuint VBO, VAO, EBO; 
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+    /* Loop until the user closes the window */
+    do
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    } while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE));
+
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
+
+    glfwTerminate();
+    return 0;
+}
